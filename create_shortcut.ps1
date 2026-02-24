d27882c53b93c2137f1de8c612c87d7af4a3a6fb
Sunil Kandola <sunil.kandola.mba19@said.oxford.edu>
2026-02-24 12:24:42 +0200

    edit to paths in create_shortcuts file

--- create_shortcut.ps1
+++ create_shortcut.ps1
@@ -2,8 +2,8 @@ $WshShell = New-Object -comObject WScript.Shell
 $DesktopPath = [Environment]::GetFolderPath("Desktop")
 $ShortcutPath = Join-Path $DesktopPath "PDF Converter.lnk"
 $Shortcut = $WshShell.CreateShortcut($ShortcutPath)
-$Shortcut.TargetPath = "c:\Users\sunil\dev\pdf-to-text\run_app.bat"
-$Shortcut.WorkingDirectory = "c:\Users\sunil\dev\pdf-to-text"
-$Shortcut.IconLocation = "c:\Users\sunil\dev\pdf-to-text\run_app.bat,0"
+$Shortcut.TargetPath = "C:\Projects\dev\pdf-to-text\run_app.bat"
+$Shortcut.WorkingDirectory = "C:\Projects\dev\pdf-to-text"
+$Shortcut.IconLocation = "C:\Projects\dev\pdf-to-text\run_app.bat,0"
 $Shortcut.Save()
 Write-Host "Shortcut created at $ShortcutPath"
