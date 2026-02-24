d8b29d939e1a8a77a4bbfc6a4fa6a180140d7a83
Sunil Kandola <sunil.kandola.mba19@said.oxford.edu>
2026-02-24 12:59:50 +0200

    Created new icon ico file. Updated file path for shortcut icon to point to new ico icon file

--- create_shortcut.ps1
+++ create_shortcut.ps1
@@ -4,6 +4,6 @@ $ShortcutPath = Join-Path $DesktopPath "PDF Converter.lnk"
 $Shortcut = $WshShell.CreateShortcut($ShortcutPath)
 $Shortcut.TargetPath = "C:\Projects\dev\pdf-to-text\run_app.bat"
 $Shortcut.WorkingDirectory = "C:\Projects\dev\pdf-to-text"
-$Shortcut.IconLocation = "C:\Projects\dev\pdf-to-text\run_app.bat,0"
+$Shortcut.IconLocation = "C:\Projects\dev\pdf-to-text\static\pdf-icon.ico"
 $Shortcut.Save()
 Write-Host "Shortcut created at $ShortcutPath"
